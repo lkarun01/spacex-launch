1a1a2e466fed13dad9c084753c40eed5df1ad9ce
lkarun01 <lalanke@yahoo.com>
2019-07-03 09:27:59 -0700

    Graph QL used to fetch data

---
+++ public/precache-manifest.1a30a43b9a1ecfbbe26f64195c7957ef.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "67f62ad360a2004e660e",
+    "url": "/static/css/main.4c04af38.chunk.css"
+  },
+  {
+    "revision": "67f62ad360a2004e660e",
+    "url": "/static/js/main.16250ff7.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "7bfdb13267986d2c5c22",
+    "url": "/static/js/2.eb69afd3.chunk.js"
+  },
+  {
+    "revision": "919a1ec52d4786d1ad0add9ab603bacb",
+    "url": "/index.html"
+  }
+];
